e899801167799ecb4461520fedb1980e56dd18c0
rolo salom <roman.salom12@gmail.com>
2024-06-04 12:58:05 -0300

    moov se ven las cards desde el axios

--- vite-project/src/components/MOOV/Card.jsx
+++ vite-project/src/components/MOOV/Card.jsx
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types';
-import '../MOOV/styles/moovCards.css';
+
 export default function CardMoov({ producto }) {
+  if (!producto) {
+    return null; // You can return null or handle this case differently based on your requirements
+  }
+
   return (
     <div className="card-container w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <img
@@ -8,25 +12,24 @@ export default function CardMoov({ producto }) {
         src={producto.imageUrl}
         alt="product image"
       />
-
       <div className="card-content px-5 pb-5">
-        <a href="#">
+        <a href="/">
           <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
             {producto.title}
           </h5>
         </a>
         <div className="flex items-center mt-2.5 mb-5">
-          <div className="flex items-center space-x-1 rtl:space-x-reverse"></div>
+          {/* Add any additional content here */}
         </div>
         <div className="flex items-center justify-between">
           <span className="text-xl font-bold text-gray-900 dark:text-white">
             {producto.price}
           </span>
           <a
-            href="#"
+            href="/"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Ver mas{' '}
+            Ver más
           </a>
         </div>
       </div>
@@ -39,5 +42,5 @@ CardMoov.propTypes = {
     imageUrl: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     price: PropTypes.string.isRequired,
-  }).isRequired,
+  }),
 };
